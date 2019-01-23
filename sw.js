a818520b4cb7f95954dba6c966bcae06fca76869
kon_yu <yusukedrive@gmail.com>
2019-01-23 18:23:45 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-49326c0594476da78476.js"
+    "url": "webpack-runtime-868134d7230adf31cc8d.js"
   },
   {
     "url": "app-100fc1e2f5e5d10eba4c.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a624e21a9546a86f224b8bad98451e6d"
+    "revision": "c20e971a09883e191ffb4ba808ddfbfa"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
