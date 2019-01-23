42ecbf3bfe48bde8f925a9fa6b310941f16dda19
kon_yu <yusukedrive@gmail.com>
2019-01-23 17:53:05 +0900

    Add: profile

--- config/website.js
+++ config/website.js
@@ -6,12 +6,12 @@ module.exports = {
   siteTitle: 'コンユウスケセイサクショ', // Navigation and Site Title
   siteTitleAlt: 'コンユウスケセイサクショ', // Alternative Site title for SEO
   siteTitleShort: 'コンユウスケセイサクショ', // short_name for manifest
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
+  siteHeadline: '', // Headline for schema.org JSONLD
   siteUrl: 'https://kon-yu.work', // Domain of your site. No trailing slash!
   siteLanguage: 'ja', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: 'ハイパフォーマンスなシステムデザインいたします',
+  author: '@kon_yu', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@kon_yu', // Twitter Username
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -63,73 +63,66 @@ const Index = () => (
     <Parallax pages={5}>
       <Hero offset={0}>
         <BigTitle>
-          Hello, <br /> I'm John Doe.
+          コンユウスケセイサクショ
         </BigTitle>
-        <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+        <Subtitle>ハイパフォーマンスなシステムデザインいたします</Subtitle>
       </Hero>
       <Projects offset={1}>
         <Title>Projects</Title>
         <ProjectsWrapper>
           <ProjectCard
-            title="Freiheit"
-            link="https://www.behance.net/gallery/58937147/Freiheit"
+            title="猫あそびインスタレーション"
+            link="https://www.youtube.com/watch?v=vynsA1DW_cA"
             bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
           >
-            This project is my entry to Adobe's #ChallengeYourPerspective contest.
+            猫は本能的に動くものにじゃれつく、じゃれつく物体にエフェクトを追加した習作
           </ProjectCard>
           <ProjectCard
-            title="Harry Potter"
-            link="https://www.behance.net/gallery/52915793/Harry-Potter"
+            title="中国拳法型の軌跡"
+            link="https://www.youtube.com/watch?v=k0q_1u9AiVc"
             bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
           >
-            I entered the DOCMA 2017 award with this Harry Potter inspired image.
+            古今東西格闘技の動きは直線的なものではない、四肢が織りなすしなやかな曲線運動を表現した
           </ProjectCard>
           <ProjectCard
-            title="Tomb Raider"
-            link="https://www.behance.net/gallery/43907099/Tomb-Raider"
+            title="ロボット掃除機から掃除取り戻す"
+            link="https://www.youtube.com/watch?v=NQ9qcvOxfJk"
             bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
           >
-            Recreation of a Tomb Raider Wallpaper (Fan Art)
+            ロボット掃除機は人間から掃き掃除という労働から開放した、逆に言うと掃き掃除という行為が機会に奪われたといえる。今度は逆にロボット掃除機を手動で制御することによりロボットの目線からの掃除を人間が獲得するものである
           </ProjectCard>
           <ProjectCard
-            title="Eagle"
-            link="https://www.behance.net/gallery/38068151/Eagle"
+            title="アンチフードポルノ"
+            link="https://www.youtube.com/watch?v=dnRT_DDRH7E"
             bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
           >
-            A fantasy image manipulation relocating the habitat of wild animals.
+            ダイエット中に食事の画像は見たくない、誰かが食べたラーメンの画像など見る必要はない。そんなフードポルノから身を護るChrome拡張機能
           </ProjectCard>
         </ProjectsWrapper>
       </Projects>
       <About offset={3}>
         <Title>About</Title>
         <AboutHero>
-          <Avatar src={avatar} alt="John Doe" />
+          <Avatar src={avatar} alt="@kon_yu" />
           <AboutSub>
-            The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-            Emoji into my speech to better express myself. Winky face.
+          フリーランスのソWEBデザイナー、メディアアーティスト、技術顧問
           </AboutSub>
         </AboutHero>
         <AboutDesc>
-          You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-          every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-          make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The kids
-          want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that all
-          right?
+          サービスでどのような体験ユーザにさせたいのか、カスターマージャーニーに沿ったシステムのデザインいたします。
         </AboutDesc>
       </About>
       <Contact offset={4}>
         <Inner>
-          <Title>Get in touch</Title>
+          <Title>Contact</Title>
           <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
+            お問い合わせはTwitterまたはFacebookのいずれかからお願いします{' '}
+            <a href="https://twitter.com/kon_yu">Twitter</a> &{' '}
+            <a href="https://www.facebook.com/yusuke.kon.79">Facebook</a>
           </ContactText>
         </Inner>
         <Footer>
-          &copy; 2019 by Gatsby Starter Portfolio Cara.{' '}
-          <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">Github Repository</a>. Made by{' '}
-          <a href="https://www.lekoarts.de">LekoArts</a>.
+          &copy; 2019 by コンユウスケセイサクショ{' '}
         </Footer>
       </Contact>
     </Parallax>
